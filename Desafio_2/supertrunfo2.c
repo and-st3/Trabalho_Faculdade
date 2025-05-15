4370ab262d5a5491d18b75eb5777a1f483a6f744
Andrews <98049668+and-st3@users.noreply.github.com>
2025-05-15 15:38:32 +0000

    initial commit

---
+++ Desafio_2/supertrunfo2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    // Primeira Carta
+    char State1[3];
+    char CardCode1[5];
+    char NameCity1[50];
+    int Population1;
+    float Area1;
+    float Pib1;
+    int TuristicPoint1;
+    float DensidadePop1;
+    float PIBperCap1;
+
+    
+}
